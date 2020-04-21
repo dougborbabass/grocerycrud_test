1385dc4f7d3cebe6e16d54b0eb7779d9d11025e0
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-04-21 11:21:56 -0300

    estrutura inicial para desafio 1

---
+++ src/test/java/tests/ResolveDesafioTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import suporte.WebSetup;
+
+public class ResolveDesafioTest {
+
+    private WebDriver navegador;
+
+    @Before
+    public void setup() {
+        navegador = WebSetup.createChromeSetup();
+    }
+
+    @Test
+    public void deveResolverDesafio(){
+
+        // Mudar o valor da combo Select Version através do id "switch-version-select"
+
+        // Clicar no botão add customer //a[@href ="/demo/bootstrap_theme_v4/add"]
+
+        // Preencher os campos do formulário
+
+        // Clicar no botão save através do id "form-button-save"
+
+        // Validar a mensagem "Your data has been successfully stored into the database." //*[@id="report-success"]/p/text()
+
+        // Fechar o browser web
+    }
+
+    @After
+    public void tearDown() {
+        navegador.quit();
+    }
+}

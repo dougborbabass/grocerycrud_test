3d286346638a5ec8f196762907d38abacd3bd0f3
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-04-23 12:00:32 -0300

    finalização das anotações @FindBy

--- src/test/java/pages/InicialPage.java
+++ src/test/java/pages/InicialPage.java
@@ -3,53 +3,66 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
 public class InicialPage extends BasePage {
+
+    @FindBy(id = "switch-version-select")
+    WebElement comboBoxVersao;
+    @FindBy(xpath = "//a[@href =\"/demo/bootstrap_theme_v4/add\"]")
+    WebElement btnAddCustomer;
+    @FindBy(xpath = "/html/body/div[3]/span[3]")
+    WebElement spanStored;
+    @FindBy(id = "gcrud-search-form")
+    WebElement formularioPesquisa;
+    @FindBy(xpath = "//*[@class=\"btn btn-outline-dark delete-selected-button\"]//span")
+    WebElement btnDelete;
+    @FindBy(xpath = "//button[contains(@class, \"delete-multiple-confirmation\")]")
+    WebElement btnConfirmDelete;
+    @FindBy(xpath = "//p[contains(.,'data has been successfully deleted')]")
+    WebElement msgDelete;
 
     public InicialPage(WebDriver navegador) {
         super(navegador);
     }
 
     public InicialPage mudarComboSelectVersion(String opcao) {
-        WebElement comboBoxVersion = navegador.findElement(By.id("switch-version-select"));
-        new Select(comboBoxVersion).selectByVisibleText(opcao);
+        new Select(comboBoxVersao).selectByVisibleText(opcao);
         return this;
     }
 
     public FormularioPage clicarAddCustomer() {
-        WebElement btnAddCustomer = navegador.findElement(By.xpath("//a[@href =\"/demo/bootstrap_theme_v4/add\"]"));
-
         mWait(btnAddCustomer).click();
         return new FormularioPage(navegador);
     }
 
     public InicialPage aguardarMsgStored() {
-        mWaitHide(navegador.findElement(By.xpath("/html/body/div[3]/span[3]")));
+        mWaitHide(spanStored);
         return this;
     }
 
     public InicialPage digitarNomePesquisa(String nome) {
-        mWait(navegador.findElement(By.id("gcrud-search-form"))).findElement(By.name("customerName")).sendKeys(nome);
+        mWait(formularioPesquisa).findElement(By.name("customerName")).sendKeys(nome);
         return this;
     }
 
     public InicialPage clicarCheckBoxLinha() {
-        mWait(navegador.findElement(By.id("gcrud-search-form"))).findElement(By.className("select-row")).click();
+        mWait(formularioPesquisa).findElement(By.className("select-row")).click();
         return this;
     }
 
     public InicialPage clicarDelete() {
-        mWait(navegador.findElement(By.xpath("//*[@class=\"btn btn-outline-dark delete-selected-button\"]//span"))).click();
+        mWait(btnDelete).click();
         return this;
     }
 
     public InicialPage confirmarDelete() {
-        mWait(navegador.findElement(By.xpath("//button[contains(@class, \"delete-multiple-confirmation\")]"))).click();
+        mWait(btnConfirmDelete).click();
         return this;
     }
 
     public String capturarMensagemSucesso() {
-        return mWait(navegador.findElement(By.xpath("//p[contains(.,'data has been successfully deleted')]"))).getText();
+        return mWait(msgDelete).getText();
     }
 }

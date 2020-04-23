d380c0907863e9f8a7f069ea79bfa040a43bb4fc
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-04-23 10:37:45 -0300

    adicionado webdriver manager e basetest.class

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage {
+abstract class BasePage {
 
     protected WebDriver navegador;
 
--- src/test/java/pages/InicialPage.java
+++ src/test/java/pages/InicialPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
 public class InicialPage extends BasePage {
